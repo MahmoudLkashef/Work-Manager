2396fdb231ec0ad3d1ecf48c2e4b0910964ebbcc
mahmoudLkashef <mahmoud.reda.e12@gmail.com>
2023-01-28 19:51:16 +0200

    Added periodic worker

--- app/src/main/java/com/example/workmanager/MainActivity.kt
+++ app/src/main/java/com/example/workmanager/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.workmanager
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.ProgressBar
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.work.*
@@ -13,29 +15,23 @@ import com.example.workmanager.chainofworkers.DownloadImageWorker
 import com.example.workmanager.chainofworkers.SaveImageWorker
 import com.example.workmanager.databinding.ActivityMainBinding
 import kotlinx.coroutines.flow.MutableStateFlow
+import java.time.Duration
 import java.util.UUID
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
     private val workManager=WorkManager.getInstance(this)
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.btnSendMessage.setOnClickListener(View.OnClickListener {
-            val downloadImageWorkRequest= OneTimeWorkRequest.from(DownloadImageWorker::class.java)
-            val applyFilterWorkRequest= OneTimeWorkRequest.from(ApplyFilterWorker::class.java)
-            val saveImageWorkRequest= OneTimeWorkRequest.from(SaveImageWorker::class.java)
-
-            //this will make a request multiple time
-/*            workManager.beginWith(downloadImageWorkRequest).then(applyFilterWorkRequest)
-                .then(saveImageWorkRequest).enqueue()*/
-
-            workManager.beginUniqueWork("image",ExistingWorkPolicy.KEEP,downloadImageWorkRequest)
-                .then(applyFilterWorkRequest)
-                .then(saveImageWorkRequest).enqueue()
+            val downloadImageWorkRequest= PeriodicWorkRequestBuilder<DownloadImageWorker>(
+                Duration.ofHours(12)
+            )
         })
 
     }
